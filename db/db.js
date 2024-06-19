10fe48c250b2c0a6fa2ecd6882f52535dd3730a5
anshul29292929 <anshulbuisness@gmail.com>
2024-06-19 15:01:31 +0530

    commit 1.2

--- db/db.js
+++ db/db.js
@@ -22,7 +22,7 @@ const { Pool } = require('pg')
 require('dotenv').config()
 
 const client = new Pool({
-    connectionString: "postgres://default:a9qcCV5npyHB@ep-red-surf-a43jgwby-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require?sslmode=require",
+    connectionString: process.env.connectionString
 })
 
 client.connect((err, result)=>{

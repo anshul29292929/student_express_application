8161e6324c2388a25ae323e2731e2efa958fa930
anshul29292929 <anshulbuisness@gmail.com>
2024-06-19 13:48:21 +0530

    commit 1.1

--- db/db.js
+++ db/db.js
@@ -1,29 +1,36 @@
-const pool = require('pg')
+// const pool = require('pg')
 
-const dotenv = require('dotenv');
+// const dotenv = require('dotenv');
 
-dotenv.config({
-    path:'../.env'
+// dotenv.config({
+//     path:'../.env'
+// })
+
+
+// console.log((process.env.dbport))
+// var config = {
+//     database: process.env.database,
+//     host: process.env.host ,
+//     password: process.env.password,
+//     port: process.env.dbport,
+//     user: process.env.user,
+// };
+
+// const client = new pool.Client(config)
+
+const { Pool } = require('pg')
+require('dotenv').config()
+
+const client = new Pool({
+    connectionString: "postgres://default:a9qcCV5npyHB@ep-red-surf-a43jgwby-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require?sslmode=require",
 })
-
-
-console.log((process.env.dbport))
-var config = {
-    database: process.env.database,
-    host: process.env.host ,
-    password: process.env.password,
-    port: process.env.dbport,
-    user: process.env.user,
-};
-
-const client = new pool.Client(config)
 
 client.connect((err, result)=>{
     if(!err){
         console.log('Database connected')
     }
     else{
-        console.log('Error in databse connection')
+        console.log('Error in databse connection', err)
     }
 })
 
